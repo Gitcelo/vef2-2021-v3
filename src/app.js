0d9bfa80dbfb04a1120a90ec95cf767d60685d6c
Gitcelo <72700939+Gitcelo@users.noreply.github.com>
2021-02-12 14:38:51 +0000

    Búinn að færa post yfir í registration

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { body, validationResult } from 'express-validator';
 import { query } from './db.js';
-import { time } from './registration.js';
+import { time, router } from './registration.js';
 
 dotenv.config();
 
@@ -10,7 +9,7 @@ const {
   PORT: port = 3000,
 } = process.env;
 
-const app = express();
+export const app = express();
 
 // TODO setja upp rest af virkni!
 
@@ -29,72 +28,16 @@ app.use(express.urlencoded({ extended: true }));
 const nationalIdPattern = '^[0-9]{6}-?[0-9]{4}$';
 
 app.get('/', async (req, res) => {
-  try{
-  const result = await query('SELECT * FROM signatures');
-  res.render('skraning', { result });
-  app.locals.listinn = null;
-  } catch(e) {
+  try {
+    const result = await query('SELECT * FROM signatures');
+    res.render('skraning', { result });
+    app.locals.listinn = null;
+  } catch (e) {
     console.error('Error selecting', e);
   }
 });
 
-app.post(
-  '/post',
-  body('nafn')
-  .isLength({ min: 1 })
-  .withMessage('Nafn má ekki vera tómt'),
-  body('nafn')
-  .isLength({ max: 128 })
-  .withMessage('Nafn má að hámarki vera 128 stafir'),
-  body('kt')
-  .isLength({ min: 1 })
-  .withMessage('Kennitala má ekki vera tóm'),
-  body('kt')
-  .matches(new RegExp(nationalIdPattern))
-  .withMessage('Kennitala verður að vera á formi 000000-0000 eða 0000000000'),
-  body('ath')
-  .isLength({ max:400 })
-  .withMessage('Athugasemd má að hámarki vera 400 stafir'),
-
-  (req, res, next) => {
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()) {
-      app.locals.listinn = errors.array().map(i => i.msg);
-      res.redirect('/');
-    }
-    else {
-      app.locals.listinn = null;
-      return next();
-    }
-  },
-
-  	body('name').trim().escape(),
-    body('kt').blacklist('-'),
-    body('ath').trim().escape(),
-
-    async (req, res) => {
-      const {
-        nafn,
-        kt,
-        ath,
-        anon,
-      } = req.body;
-
-      let result;
-
-      try {
-      if(anon === 'on')
-        result = await query('INSERT INTO signatures (name, nationalID, comment) VALUES ($1, $2, $3)', ['Nafnlaust', kt, ath]);
-      else
-          result = await query('INSERT INTO signatures (name, nationalID, comment, anonymous) VALUES ($1, $2, $3, $4)', [nafn, kt, ath, false]);
-        res.redirect('/');
-      } catch(e) {
-        res.render('villa');
-      }
-    }
-
-  );
+app.post('/post', router);
 
 // Verðum að setja bara *port* svo virki á heroku
 app.listen(port, () => {
--- src/registration.js
+++ src/registration.js
@@ -1,15 +1,73 @@
 import express from 'express';
+import { app } from './app.js'
+import { body, validationResult } from 'express-validator';
+import { query } from './db.js';
 
 // TODO skráningar virkni
-const app = express();
+const nationalIdPattern = '^[0-9]{6}-?[0-9]{4}$';
 
-export function existing(val){
-  if(val) return true;
-  return false;
+export function time(d) {
+  return `${d.getDate()}.${d.getMonth() + 1}.${d.getFullYear()}`
 }
 
-export function time(d){
-  return `${d.getDate()}.${d.getMonth()+1}.${d.getFullYear()}`
+export const router = express.Router();
+
+router.post('/post',
+  body('nafn')
+    .isLength({ min: 1 })
+    .withMessage('Nafn má ekki vera tómt'),
+  body('nafn')
+    .isLength({ max: 128 })
+    .withMessage('Nafn má að hámarki vera 128 stafir'),
+  body('kt')
+    .isLength({ min: 1 })
+    .withMessage('Kennitala má ekki vera tóm'),
+  body('kt')
+    .matches(new RegExp(nationalIdPattern))
+    .withMessage('Kennitala verður að vera á formi 000000-0000 eða 0000000000'),
+  body('ath')
+    .isLength({ max: 400 })
+    .withMessage('Athugasemd má að hámarki vera 400 stafir'),
+
+  (req, res, next) => { valid(req, res, next); },
+
+  body('name').trim().escape(),
+  body('kt').blacklist('-'),
+  body('ath').trim().escape(),
+
+  async (req, res) => { insertion(req, res); }
+)
+
+function valid(req, res, next) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    app.locals.listinn = errors.array().map(i => i.msg);
+    res.redirect('/');
+  }
+  else {
+    app.locals.listinn = null;
+    return next();
+  }
 }
 
+async function insertion(req, res) {
+  const {
+    nafn,
+    kt,
+    ath,
+    anon,
+  } = req.body;
 
+  let result;
+
+  try {
+    if (anon === 'on')
+      result = await query('INSERT INTO signatures (name, nationalID, comment) VALUES ($1, $2, $3)', ['Nafnlaust', kt, ath]);
+    else
+      result = await query('INSERT INTO signatures (name, nationalID, comment, anonymous) VALUES ($1, $2, $3, $4)', [nafn, kt, ath, false]);
+    res.redirect('/');
+  } catch (e) {
+    res.render('villa');
+  }
+}
